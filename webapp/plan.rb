fb84a76f7e135a137a352438107f0fbad09ea6f2
Lucas Florio <lucasefe@gmail.com>
2015-01-02 12:57:08 -0300

    Add bootstrap and update boilerplate code.

--- webapp/plan.rb
+++ webapp/plan.rb
@@ -12,15 +12,23 @@ module Plan
   def name
     "hellapp"
   end
-  
-  template "AppServer",   "#{Plan.name}/lib/#{Plan.name}/app.rb"
-  template "AppRoot",     "#{Plan.name}/lib/#{Plan.name}.rb"
-  template "ConfigRu",    "#{Plan.name}/config.ru"
-  template "Env",         "#{Plan.name}/.env"
-  template "Gemfile",     "#{Plan.name}/Gemfile"
-  template "GitIgnore",   "#{Plan.name}/.gitignore"
-  template "Procfile",    "#{Plan.name}/Procfile"
-  template "Rakefile",    "#{Plan.name}/Rakefile"
-  template "RubyVersion", "#{Plan.name}/.ruby-version"
+
+  template "AppIndex",        "#{Plan.name}/lib/#{Plan.name}/views/index.slim"
+  template "AppInitializer",  "#{Plan.name}/initializers/001_load.rb"
+  template "AppJs",           "#{Plan.name}/public/assets/javascripts/app.js"
+  template "AppLayout",       "#{Plan.name}/lib/#{Plan.name}/views/layout.slim"
+  template "AppRoot",         "#{Plan.name}/lib/#{Plan.name}.rb"
+  template "AppRoute",        "#{Plan.name}/lib/#{Plan.name}/route.rb"
+  template "AppServer",       "#{Plan.name}/lib/#{Plan.name}/app.rb"
+  template "BootstrapCss",    "#{Plan.name}/public/assets/stylesheets/bootstrap.min.css"
+  template "BootstrapJs",     "#{Plan.name}/public/assets/javascripts/bootstrap.min.js"
+  template "ConfigRu",        "#{Plan.name}/config.ru"
+  template "Env",             "#{Plan.name}/.env"
+  template "Gemfile",         "#{Plan.name}/Gemfile"
+  template "GitIgnore",       "#{Plan.name}/.gitignore"
+  template "JqueryJs",        "#{Plan.name}/public/assets/javascripts/jquery.min.js"
+  template "Procfile",        "#{Plan.name}/Procfile"
+  template "Rakefile",        "#{Plan.name}/Rakefile"
+  template "RubyVersion",     "#{Plan.name}/.ruby-version"
 
 end
